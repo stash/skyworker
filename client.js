bb9c041afd7829bdcd6b79cf57f314183f22007e
Jordan Patterson <bigfoot13442@gmail.com>
2012-09-29 14:27:29 -0300

    fixed a screwup

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
+function say(msg)
+{
+	worker.postMessage({'msg': msg});
+}
+
 var blob = new Blob(["onmessage = function(e) { postMessage(e.data.msg); };"]);
-var windowURL = window.URL || window.webkitURL
+var windowURL = window.URL || window.webkitURL;
 // Obtain a blob URL reference to our worker 'file'.
 var blobURL = windowURL.createObjectURL(blob);
 
@@ -8,5 +13,11 @@ var worker = new Worker(blobURL);
 worker.onmessage = function(e) {
   document.getElementById('result').textContent = e.data;
 };
+
+/*var socket = io.connect('http://localhost:8123');
+socket.on('news', function (data) {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});*/
 
 worker.postMessage({'msg': 'worker started'});
