2693e2bc3164c11f5be081ce96054db32fade51e
Jordan Patterson <bigfoot13442@gmail.com>
2012-09-29 14:39:27 -0300

    added socket.io client an server example

--- client.js
+++ client.js
@@ -14,10 +14,10 @@ worker.onmessage = function(e) {
   document.getElementById('result').textContent = e.data;
 };
 
-/*var socket = io.connect('http://localhost:8123');
+var socket = io.connect('http://localhost:8123');
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
-});*/
+});
 
 worker.postMessage({'msg': 'worker started'});
--- server.js
+++ server.js
@@ -36,5 +36,12 @@ io.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling']);
 io.set('io name', 'skyworker.io');
 io.disable('match origin protocol');
 
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 server.listen(8123);
 
